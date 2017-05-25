c85b936f5ce3666346806ea2a2330ca93dbdc6de
metraf <firsov.tyoma@gmail.com>
2017-05-26 00:16:41 +0200

    added static files

---
+++ src/main/resources/static/js/prod.js
@@ -0,0 +1,85 @@
+/**
+ * Created by metraf on 11.12.16.
+ */
+var productsURL = "http://localhost:8080/api/products";
+var app = angular.module('myApp', ["angucomplete"]);
+
+app.controller('productsCtrl', function ($scope, $http) {
+    $http.get(productsURL)
+        .then(function (response) {
+            // for(var i = 0; i < response.data.length; i++){
+            //     response.data[i].carbo = response.data[i].carbo.toFixed(2);
+            //     response.data[i].protein = response.data[i].protein.toFixed(2);
+            //     response.data[i].fat = response.data[i].fat.toFixed(2);
+            //     response.data[i].kcal = response.data[i].kcal.toFixed(2);
+            // }
+            $scope.weight = 0;
+            $scope.products = response.data;
+            $scope.ration = [];
+            $('#rationCarbo').text('0.00');
+            $('#rationProtein').text('0.00');
+            $('#rationFat').text('0.00');
+            $('#rationKcal').text('0.00');
+            $scope.addRation = function () {
+                $scope.ration.push({
+                    weight: $scope.weight,
+                    protein: (parseFloat($('#selectedProtein').val()) * $scope.weight).toFixed(2),
+                    fat: (parseFloat($('#selectedFat').val()) * $scope.weight).toFixed(2),
+                    carbo: (parseFloat($('#selectedCarbo').val()) * $scope.weight).toFixed(2),
+                    kcal: (parseFloat($('#selectedKcal').val()) * $scope.weight).toFixed(2),
+                    name: firstToUpperCase($('#selectedName').val()),
+                    counter: $scope.ration.length + 1
+                });
+                $('#name_value').val('');
+                $scope.weight = 0;
+                // console.log($scope.ration);
+            }
+            $scope.removeItem = function (x) {
+                $scope.ration.splice(x, 1);
+                for (var i = 0; i < $scope.ration.length; i++) {
+                    $scope.ration[i].counter = i + 1;
+                }
+            }
+            $scope.saveNewProducts = function () {
+                if ($scope.newProducts.$valid) {
+                    // console.log($scope.newProducts);
+                    var data = {
+                        protein: $scope.newProd.protein / 100,
+                        fat: $scope.newProd.fat / 100,
+                        carbo: $scope.newProd.protein / 100,
+                        kcal: $scope.newProd.kcal / 100,
+                        name: $scope.newProd.name.toLowerCase()
+                    };
+                    if (data.protein + data.fat + data.carbo < 1) {
+
+                        // console.log(data);
+                        $scope.newProd.protein = "";
+                        $scope.newProd.fat = "";
+                        $scope.newProd.carbo = "";
+                        $scope.newProd.name = "";
+                        $scope.newProd.kcal = "";
+
+                        $http.post(productsURL, data)
+                            .then(function (response) {
+                                $('#modal').modal('toggle');
+                                $scope.errorSum = "";
+                                $http.get(productsURL).then(function (response) {
+                                    $scope.products = response.data;
+                                });
+                            });
+                    }
+                    else {
+                        $scope.newProducts.newProtein.$invalid = true;
+                        $scope.newProducts.newCarbo.$invalid = true;
+                        $scope.newProducts.newFat.$invalid = true;
+                        $scope.errorSum = " *Die Summe von der drei Felder muss nicht größer als 100g sein.";
+                    }
+
+                }
+            }
+        });
+});
+
+function firstToUpperCase( str ) {
+    return str.substr(0, 1).toUpperCase() + str.substr(1);
+}
---
+++ src/main/resources/static/js/usersValid.js
@@ -0,0 +1,106 @@
+/**
+ * Created by metraf on 25.12.16.
+ */
+var app = angular.module('valid', []);
+
+app.controller('validRegist', function ($scope) {
+    var checkName = new getAjax();
+    $('#registLogin').keyup(function () {
+
+        checkName.getArrayOptions.url = 'api/checkNewName/' + $(this).val();
+        checkName.getArrayOptions.async = false;
+        console.log($scope.registForm.registLogin.$invalid);
+        var arr = checkName.getArray();
+        var error = new uniqFunc();
+        if (arr) {
+            // console.log('not uniq');
+            // $scope.registForm.registLogin.$invalid = true;
+            // $scope.registForm.registLogin.$error.notUniq = true;
+            error.setError();
+        }
+        else {
+            if ($.trim($(this).val()).length > 3) {
+                // console.log('bla');
+                // $scope.registForm.registLogin.$invalid = false;
+                // $scope.registForm.registLogin.$error.notUniq = false;
+                error.setSuccess();
+            }
+        }
+    });
+    $scope.saveNewUser = function () {
+        // console.log('test');
+        if ($scope.registForm.$valid) {
+            var data = {
+                name: $scope.regist.login.toLowerCase(),
+                email: $scope.regist.email,
+                password: $scope.regist.password
+            };
+            console.log(JSON.stringify(data));
+        }
+    };
+});
+
+app.directive('pwCheck', [function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            var firstPassword = '#' + attrs.pwCheck;
+            elem.add(firstPassword).on('keyup', function () {
+                scope.$apply(function () {
+                    var v = elem.val() === $(firstPassword).val();
+                    ctrl.$setValidity('pwmatch', v);
+                });
+            });
+        }
+    }
+}]);
+var uniqFunc = function () {
+  this.elements = {
+      name: $('#registLogin'),
+      button: $('#reg_but'),
+      errorMes: $('#errorName')
+  };
+  this.setError = function () {
+      this.elements.name.parent().addClass('has-error');
+      this.elements.errorMes.removeClass('hidden');
+      // this.elements.button.disable(true);
+      this.elements.button.attr('disabled', true);
+  };
+  this.setSuccess = function () {
+      this.elements.name.parent().removeClass('has-error');
+      this.elements.errorMes.addClass('hidden');
+      // this.elements.button.attr('disabled', false);
+  };
+};
+
+var getAjax = function () {
+    this.getArrayOptions = {
+        url: "/api",
+        contentType: "application/json",
+        async: true
+    };
+    this.getArray = function () {
+        var arr;
+        $.ajax({
+            async: this.getArrayOptions.async,
+            url: this.getArrayOptions.url,
+            type: 'GET',
+            dataType: 'html',
+            contentType: this.getArrayOptions.contentType,
+            success: function (data) {
+                if (isJson(data)) {
+                    arr = JSON.parse(data);
+                }
+            }
+        });
+        return arr;
+    }
+};
+function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
